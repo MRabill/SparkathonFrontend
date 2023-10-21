ee654cbbfbae8c5ba3b12a0f4c624e907f1671f5
rabill.madarboccus <rabill.madarboccus@rogerscapital.mu>
2023-10-21 14:58:33 +0400

    Add dashboard to /

--- src/main.jsx
+++ src/main.jsx
@@ -1,50 +1,46 @@
-import React, { lazy, Suspense }  from 'react'
-import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from "react-query";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React, { lazy, Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './styles/main.css';
-
 
 const queryClient = new QueryClient();
 
-const FirstPage = lazy(() => import('./pages/Firstpage'))
-const SecondPage = lazy(() => import('./pages/SecondPage'))
-const ThirdPage = lazy(() => import('./pages/ThirdPage'))
-const SpringModal = lazy(() => import('./pages/SpringModal'))
-const Dashboard = lazy(() => import('./pages/Dashboard'))
-const GameOasis = lazy(() => import('./pages/GameOasis'))
+const FirstPage = lazy(() => import('./pages/Firstpage'));
+const SecondPage = lazy(() => import('./pages/SecondPage'));
+const ThirdPage = lazy(() => import('./pages/ThirdPage'));
+const SpringModal = lazy(() => import('./pages/SpringModal'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const GameOasis = lazy(() => import('./pages/GameOasis'));
 
 const Loading = () => {
-  return <div>Loading...</div>; 
+  return <div>Loading...</div>;
 };
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <FirstPage/>,
+    path: '/',
+    element: <Dashboard />,
   },
   {
-    path: "/first",
-    element: <SpringModal/>,
+    path: '/first',
+    element: <SpringModal />,
   },
   {
-    path: "/second",
-    element: <SecondPage/>,
+    path: '/second',
+    element: <SecondPage />,
   },
   {
-    path: "/dashboard",
-    element: <Dashboard/>,
+    path: '/dashboard',
+    element: <Dashboard />,
   },
   {
-    path: "/oasis",
-    element: <GameOasis/>,
+    path: '/oasis',
+    element: <GameOasis />,
   },
   {
-    path: "/third/:id",
-    element: <ThirdPage/>,
+    path: '/third/:id',
+    element: <ThirdPage />,
   },
   // {
   //   path: "/react_front_template",
@@ -63,16 +59,12 @@ const router = createBrowserRouter([
   // }
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-    <Suspense fallback={<Loading />}>
-    <RouterProvider router={router} /> 
-</Suspense>
-
+      <Suspense fallback={<Loading />}>
+        <RouterProvider router={router} />
+      </Suspense>
     </QueryClientProvider>
-    
   </React.StrictMode>
 );
-
-
